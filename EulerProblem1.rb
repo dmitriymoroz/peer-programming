01f53a713b0ddb9c2210e25a4105fcfd424bea04
Derrick Coleman-Turner <derrickcoleman-turner@aptrents-MacBook-Air.local>
2013-04-19 21:52:05 -0500

    Added several instances of IF statements

--- EulerProblem1.rb
+++ EulerProblem1.rb
@@ -5,9 +5,11 @@ class DivideValue
 
   def self.can_divide? numerator, denominator
     if numerator % denominator == 0
+      puts "Divide = True"
       return true
       #puts "this is in the CanDivide Function"
     end
+    puts "Divide = False"
     return false
   end
 
@@ -16,19 +18,37 @@ end
 @counter = 0
 
 for i in 1..10 do
-=begin
-  if i % 3 == 0
-    @counter += i
-    puts @counter
-    #puts "hello"
-  end
-=end
-   DivideValue.can_divide? i, 3 | @counter += i
 
-  puts "i" + i.to_s
-  #dv = DivideValue.new
-  #dv.can_divide? i, 3
-    puts @counter
+  #string outputs for testing
+  puts "i value: " + i.to_s
+
+#Original If Statement
+#  if i % 3 == 0
+#    @counter += i
+#    puts @counter
+#    #puts "hello"
+#  end
+
+#simple if/end: VALID
+#if DivideValue.can_divide? i, 3 
+#  @counter += i
+#end
+
+#if with return values: VALID
+#  @counter = if DivideValue.can_divide? i, 3 
+#              @counter + i
+#            else
+#              @counter
+#            end
+  
+#conditional operators: INVALID
+  #DivideValue.can_divide? i, 3  ? (@counter += i) : @counter
+
+#if statement modifier: VALID
+  @counter += i if DivideValue.can_divide? i, 3 
+
+
+  puts "counter value: " + @counter.to_s
 
 end
 puts @counter
